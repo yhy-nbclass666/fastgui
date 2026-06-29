3776b1d3a33d5433343e1ffa0f12469ecac295e9
yhy <18309019556@163.com>
2026-06-29 23:30:11 +0800

    Complete the construction of the infrastructure.

---
+++ fastgui/__init__.py
@@ -0,0 +1,10 @@
+"""
+fastgui v0.1.0
+
+A Python GUI library developed by a sixth-grade elementary 
+school student in China. It might just be a casual project, 
+but I hope I won't forget about it when summer vacation comes. 
+Uploading to GitHub in China is too slow.
+"""
+
+__version__ = "0.1.0"
